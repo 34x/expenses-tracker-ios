3e001fe45f947347c2a1d4a3275f2405d6affb10
34x <maks@34x.me>
2019-11-30 20:04:04 +0100

    Initial Commit

---
+++ ExpencesTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpencesTracker
+//
+//  Created by Maksim Tuzhilin on 30.11.19.
+//  Copyright © 2019 Maksim Tuzhilin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
