176d437aa61a200734e5ee68d99f98f39e2911cf
34x <maks@34x.me>
2020-03-26 12:03:44 +0100

    Fix typo in test files (rename)

--- ExpensesTrackerTests/ExpensesTrackerTests.swift
+++ ExpensesTrackerTests/ExpensesTrackerTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 //@testable import ExpencesTracker
 
-class ExpencesTrackerTests: XCTestCase {
+class ExpensesTrackerTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- ExpensesTrackerUITests/ExpensesTrackerUITests.swift
+++ ExpensesTrackerUITests/ExpensesTrackerUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class ExpencesTrackerUITests: XCTestCase {
+class ExpensesTrackerUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
